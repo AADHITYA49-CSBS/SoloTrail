776a72aadf16864eea47154cada53d9fdc539537
Aadhitya.S <150918136+AADHITYA49-CSBS@users.noreply.github.com>
2026-05-22 15:28:49 +0530

    Created AuthService with user registration logic

---
+++ src/main/java/com/solotrail/service/AuthService.java
@@ -0,0 +1,32 @@
+package com.solotrail.service;
+
+import com.solotrail.dto.RegisterRequest;
+import com.solotrail.entity.User;
+import com.solotrail.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public String registerUser(RegisterRequest request) {
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            return "Email already exists!";
+        }
+
+        User user = new User();
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+
+        userRepository.save(user);
+
+        return "User registered successfully!";
+    }
+}
+
