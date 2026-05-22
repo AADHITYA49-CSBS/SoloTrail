55185cb8743b9445b61c275af8d0342e1ed0f063
Aadhitya.S <150918136+AADHITYA49-CSBS@users.noreply.github.com>
2026-05-22 13:01:21 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/solotrail/SolotrailBackendApplication.java
@@ -0,0 +1,13 @@
+package com.solotrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolotrailBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolotrailBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solotrail/SolotrailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solotrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolotrailBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
